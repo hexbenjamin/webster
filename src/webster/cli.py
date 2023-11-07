37defed93008cdc28e27c1444198014014a9d09b
hex benjamin <hex@hexbenjam.in>
2023-11-06 20:58:04 -0500

    add html2text to dependencies and update content hash in pdm.lock

--- src/webster/cli.py
+++ src/webster/cli.py
@@ -1,12 +1,23 @@
 import asyncio
 import click
 
+from rich.traceback import install
+
 from webster import __version__ as version, log
 from webster.scrape import Scraper
 
 
 @click.group()
-def cli():
+def cli() -> None:
+    """
+    entrypoint for the webster CLI.
+
+    args:
+        None
+
+    returns:
+        None
+    """
     pass
 
 
@@ -16,17 +27,36 @@ def cli():
     "--depth",
     "-d",
     default=3,
-    help="The maximum depth to scrape. Defaults to 3.",
+    help="the maximum depth to scrape. Defaults to 3.",
 )
 @click.option(
     "--include",
     "-i",
     multiple=True,
-    help="Only scrape URLs that start with this path. Can be used multiple times.",
+    help="only scrape URLs that start with this path. can be used multiple times.",
 )
-def scrape(url: str, depth: int, include: list = None) -> None:
+@click.option(
+    "--tag-name", "-t", help="only scrape the first tag with this name on the page."
+)
+@click.option(
+    "--out-format",
+    "-f",
+    help="the output format for saving scraped content. accepts 'html' or 'md'.",
+)
+def scrape(
+    url: str,
+    depth: int,
+    include: list = None,
+    tag_name: str = None,
+    out_format: str = "html",
+) -> None:
     """
-    Scrape a website and save the HTML files locally. Depth is the maximum number of link-outs to follow from the URL.
+    scrape a website and save the content locally. depth is the maximum number of link-outs to follow from the URL.
+
+    args:
+        url (str): the URL to scrape.
+        depth (int): the maximum depth to scrape.
+        include (list): a list of paths to include. defaults to None.
     """
 
     if include is None:
@@ -34,12 +64,34 @@ def scrape(url: str, depth: int, include: list = None) -> None:
     else:
         include = [i if i.startswith("/") else f"/{i}" for i in include]
 
-    scraper = Scraper(site=url, depth=depth, include_paths=include)
+    if out_format.lower() not in {"html", "md"}:
+        log(
+            "warn",
+            "invalid output format!",
+            "defaulting to 'html'.",
+            marker=False,
+            label=True,
+        )
+        out_format = "html"
+
+    scraper = Scraper(
+        site=url,
+        depth=depth,
+        include_paths=include,
+        tag_filter={"name": tag_name} if tag_name else None,
+        output_format=out_format,
+    )
 
     asyncio.run(scraper.run())
 
 
-def run():
+def run() -> None:
+    """
+    entrypoint for the 'webster' command-line script.
+    """
+
+    install(show_locals=True)
+
     print("")
-    log("note", f"webster v{version}", "is online!")
+    log("success", "webster", f"v{version}")
     cli()
--- src/webster/scrape.py
+++ src/webster/scrape.py
@@ -4,11 +4,18 @@ import random
 import aiohttp
 import asyncio
 import re
+
 from urllib.parse import urlparse
 from collections import defaultdict
-from bs4 import BeautifulSoup
+
+from typing import Union
+
+from bs4 import BeautifulSoup, NavigableString, Tag
+import html2text
 
 from webster import log
+
+from rich import inspect
 
 
 USER_AGENTS = [
@@ -25,23 +32,129 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
 ]
 
+HTML_IO = html2text.HTML2Text()
+
 
 def clean_url(url: str) -> str:
+    """
+    Clean the URL by replacing slashes, dots, and colons with dashes.
+
+    args:
+        url (str): the URL to be cleaned.
+
+    returns:
+        str: the cleaned URL.
+
+    """
+
     return re.sub(r"/|\.|:", "-", url).removesuffix("-")
 
 
-def extract_article(soup: BeautifulSoup):
-    return soup.find("article")
+def extract_tag(soup: BeautifulSoup, **kwargs) -> Union[Tag, NavigableString, None]:
+    """
+    extract and return the first tag that matches the given keyword arguments from the BeautifulSoup object.
+
+    args:
+        soup (BeautifulSoup): the BeautifulSoup object to search in.
+        **kwargs: keyword arguments specifying the attributes and values to match. these will be passed to the 'soup.find()' method.
+
+    returns:
+        Tag or None: the first matching tag, or None if no match is found.
+
+    """
+    return soup.find(**kwargs)
+
+
+def parse_markdown(tag: BeautifulSoup) -> str:
+    """
+    parse the given BeautifulSoup tag as Markdown and return the resulting HTML as a string.
+
+    args:
+        tag (BeautifulSoup): The BeautifulSoup tag to parse.
+
+    returns:
+        str: the parsed Markdown content as HTML.
+
+    """
+    return HTML_IO.handle(tag)
+
+
+def decompose_media(tag: BeautifulSoup) -> BeautifulSoup:
+    """
+    decompose the given BeautifulSoup tag and return the resulting tag.
+
+    args:
+        tag (BeautifulSoup): The BeautifulSoup tag to parse.
+
+    returns:
+        BeautifulSoup: the decomposed tag.
+
+    """
+    for media in (
+        tag.find_all("img") + tag.find_all("video") + tag.find_all(class_="breadcrumbs")
+    ):
+        media.decompose()
+    return tag
 
 
 class Scraper:
-    def __init__(self, site, depth, include_paths=None):
+    """
+    A web scraper for scraping web pages and processing links.
+
+    args:
+        site (str): the URL of the web page to scrape.
+        depth (int): the maximum depth for recursive link processing.
+        include_paths (list, optional): list of paths to include for link processing.
+        tag_filter (dict, optional): dictionary of tag attributes to filter during scraping.
+        output_format (str, optional): the output format for saving scraped content. must be "html" or "md". defaults to "html".
+    """
+
+    def __init__(
+        self,
+        site: str,
+        depth: int,
+        include_paths: list = None,
+        tag_filter: dict = None,
+        output_format: str = None,
+    ) -> None:
+        """
+        Initialize the Scraper object.
+
+        args:
+            site (str): The URL of the web page to scrape.
+            depth (int): The maximum depth for recursive link processing.
+            include_paths (list, optional): List of paths to include for link processing.
+            tag_filter (dict, optional): Dictionary of tag attributes to filter during scraping.
+            output_format (str, optional): The output format for saving scraped content.
+
+        returns:
+            None
+        """
+
         self.site = site
         self.depth = depth
         self.includes = include_paths
+        self.tag_filter = tag_filter or {"name": "body"}
+        self.output_format = (
+            output_format.lower() if output_format.lower() in {"html", "md"} else "html"
+        )
         self.sitemap = defaultdict(lambda: "")
 
     async def fetch(self, url: str, session: aiohttp.ClientSession) -> str:
+        """
+        Fetch the content of the given URL using the provided aiohttp ClientSession.
+
+        args:
+            url (str): The URL to fetch.
+            session (aiohttp.ClientSession): The aiohttp ClientSession to use for the request.
+
+        returns:
+            str: The content of the response as a string, or None if an error occurs.
+
+        Raises:
+            aiohttp.ClientError: If an error occurs during the request.
+        """
+
         headers = {"User-Agent": random.choice(USER_AGENTS)}
         try:
             async with session.get(url, headers=headers) as response:
@@ -57,11 +170,43 @@ class Scraper:
             )
             return None
 
-    async def save_response_content(self, content: str, path: str):
+    async def save_output(self, content: str, path: str) -> None:
+        """
+        Save the given content to the given path.
+
+        args:
+            content (str): The content to save.
+            path (str): The path to save the content to.
+
+        returns:
+            None
+        """
+
         with open(path, "w", encoding="utf-8") as f:
             f.write(content)
 
-    async def process_links(self, links, session, scheme, origin, depth):
+    async def process_links(
+        self,
+        links: list,
+        session: aiohttp.ClientSession,
+        scheme: str,
+        origin: str,
+        depth: int,
+    ) -> None:
+        """
+        Process the links found in the web page.
+
+        args:
+            links (list): a list of BeautifulSoup tags representing the links.
+            session (aiohttp.ClientSession): the aiohttp ClientSession to use for making requests.
+            scheme (str): the URL scheme [e.g., "http", "https"].
+            origin (str): the URL origin.
+            depth (int): the maximum depth for recursive link processing.
+
+        returns:
+            None
+        """
+
         tasks = []
         for link in links:
             href = urlparse(link.get("href", ""))
@@ -80,14 +225,36 @@ class Scraper:
             )
         await asyncio.gather(*tasks)
 
-    async def scrape(self, session, scheme, origin, path, depth):
+    async def scrape(
+        self,
+        session: aiohttp.ClientSession,
+        scheme: str,
+        origin: str,
+        path: str,
+        depth: int,
+    ) -> None:
+        """
+        scrape the web page at the given URL and process the links recursively.
+
+        args:
+            session: the aiohttp ClientSession to use for making requests.
+            scheme (str): the URL scheme [e.g., "http", "https"].
+            origin (str): the URL origin.
+            path (str): the URL path.
+            depth (int): the maximum depth for recursive link processing.
+
+        returns:
+            None
+        """
+
         site_url = f"{scheme}://{origin}{path}"
-        cleaned_url = self.clean_url(site_url)
+        cleaned_url = clean_url(origin + path)
 
         if (
             depth < 0
             or self.sitemap[cleaned_url] != ""
             or not any(path.startswith(p) for p in self.includes)
+            or site_url == ""
         ):
             return
 
@@ -96,23 +263,37 @@ class Scraper:
         if response_content is None:
             return
 
-        if not os.path.exists("./scrape"):
-            os.mkdir("./scrape")
-        await self.save_response_content(
-            response_content, f"./scrape/{cleaned_url}.html"
-        )
-
         soup = BeautifulSoup(response_content, "html.parser")
-        if self.extract_article(soup):
-            log("note", "article found!")
-        else:
-            log("warn", "no article found!")
+
+        if output := extract_tag(soup, **self.tag_filter):
+            output = decompose_media(output).decode()
+            if self.output_format == "md":
+                output = parse_markdown(output)
+
+            if not os.path.exists("./scrape"):
+                os.makedirs("./scrape")
+
+            await self.save_output(
+                output,
+                f"./scrape/{cleaned_url}.{self.output_format}",
+            )
 
         await self.process_links(soup.find_all("a"), session, scheme, origin, depth)
 
-    async def run(self):
+    async def run(self) -> None:
+        """
+        run the web scraping process.
+
+        args:
+            None
+
+        returns:
+            None
+
+        """
+
         url = urlparse(self.site)
-        log("note", "scraping initiated!" f"for: {self.site} @ depth: {self.depth}\n")
+        log("note", "scraping initiated!", f"for: {self.site} @ depth: {self.depth}\n")
 
         async with aiohttp.ClientSession() as session:
             await self.scrape(session, url.scheme, url.netloc, url.path, self.depth)
