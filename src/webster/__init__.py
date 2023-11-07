116293663bce03dfe08fbd80d113838d8b5d0852
hex benjamin <hex@hexbenjam.in>
2023-11-07 04:55:50 -0500

    import `Embedder` and `Scraper` classes in package

--- src/webster/__init__.py
+++ src/webster/__init__.py
@@ -7,5 +7,8 @@ GitHub: https://dev.hexbenjam.in/webster
 __version__ = "0.2.0"
 
 from webster.console import C, C_LOG as log
+from webster.embed import Embedder
+from webster.scrape import Scraper
 
-__all__ = ["__version__", "C", "log"]
+
+__all__ = ["__version__", "C", "log", "Embedder", "Scraper"]
